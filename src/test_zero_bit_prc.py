063c38ec3eef4d4a8d8df5c703b023dcb1cfb591
Xander Coomes <xander.coomes@gmail.com>
2025-08-05 13:53:32 -0700

    adding key saving in testing

---
+++ src/test_zero_bit_prc.py
@@ -0,0 +1,63 @@
+from zero_bit_prc import ZeroBitPRC
+import numpy as np
+import galois
+import pytest
+import os
+
+GF = galois.GF(2)
+
+#Testing Parameters
+noise_levels = np.linspace(0, 0.09, 10)
+noise_levels = noise_levels.round(2)
+n = 2 ** 9
+
+
+@pytest .mark.parametrize("noise", noise_levels)
+def test_encode_with_noise(noise: float, codeword_len: int = n): 
+    PRC = ZeroBitPRC(codeword_len)
+    generator_matrix, parity_check_matrix, one_time_pad = fetch_keys(codeword_len)
+    encoding_key = generator_matrix, one_time_pad
+    decoding_key = parity_check_matrix, one_time_pad
+    codeword = PRC.Encode(encoding_key, noise)
+    watermarked = PRC.Decode(decoding_key, codeword)
+    assert (watermarked == True), "Parity checks exceeded threshold"
+
+def empirical_false_positive_rate(codeword_len = n, num_trials = 1000): 
+    PRC = ZeroBitPRC(codeword_len, 0)
+    generator_matrix, parity_check_matrix, one_time_pad = PRC.KeyGen()
+    decoding_key = parity_check_matrix, one_time_pad
+    false_positive_count = 0
+    for _ in range(num_trials):
+        random_codeword = GF.random(codeword_len)
+        watermarked = PRC.Decode(decoding_key, random_codeword)
+        if not watermarked:
+            false_positive_count += 1
+    false_positive_rate =  (false_positive_count / num_trials)
+    print(false_positive_rate)
+    
+
+
+#Helper Methods
+
+def fetch_keys(codeword_len: int):
+    save_dir = "keys" 
+    os.makedirs(save_dir, exist_ok = True)
+    filename = os.path.join(save_dir, f"keys_n{codeword_len}.npz")
+
+    if os.path.exists(filename):
+        print(f"[+] Loading keys for n={codeword_len}")
+        data = np.load(filename)
+        g = data["generator_matrix"]
+        p = data["parity_check_matrix"]
+        otp = data["one_time_pad"]
+    else:
+        print(f"[!] No keys found for n={codeword_len}, generating new ones...")
+        PRC = ZeroBitPRC(codeword_len)
+        g, p, otp = PRC.KeyGen()
+        np.savez(filename,
+                 generator_matrix=g,
+                 parity_check_matrix=p,
+                 one_time_pad=otp)
+        print(f"[+] Saved new keys to {filename}")
+    
+    return GF(g), GF(p), GF(otp)
---
+++ src/zero_bit_prc.py
@@ -0,0 +1,64 @@
+#Note, parts of code is borrowed & inspired by Xuandong Zhao and Sam Gunn's work on PRCs, their github is linked in references
+import numpy as np
+import galois
+
+GF = galois.GF(2)
+
+class ZeroBitPRC(): 
+    def __init__(self, codeword_len: int):
+        self.codeword_len = codeword_len
+        self.sparsity = int(np.log2(codeword_len))
+        self.secret_len = self.sparsity ** 2
+        self.num_parity_checks = int(0.99 * codeword_len)
+
+    def KeyGen(self):
+        parity_check_matrix = np.zeros((self.num_parity_checks, self.codeword_len), dtype = int)
+        for i in range (self.num_parity_checks):
+            row = np.zeros(self.codeword_len, dtype=int)
+            ones_indices = np.random.choice(self.codeword_len, size = self.sparsity, replace = False)
+            row[ones_indices] = 1
+            parity_check_matrix[i] = row 
+
+        parity_check_matrix = GF(parity_check_matrix)
+        null_space = parity_check_matrix.null_space()
+        null_space = null_space.T 
+
+        generator_matrix = np.zeros((self.codeword_len, self.secret_len), dtype = int)
+        for i in range (self.secret_len): 
+            rand_null_vector = null_space @ GF.Random(null_space.shape[1])
+            generator_matrix[:, i] = rand_null_vector
+        
+        generator_matrix = GF(generator_matrix)
+        one_time_pad = GF.Random(self.codeword_len)
+        
+        return generator_matrix, parity_check_matrix, one_time_pad
+        
+    def Encode(self, encoding_key, noise_rate: float):
+        generator_matrix, one_time_pad = encoding_key
+        secret = GF.Random(self.secret_len)
+        error = GF(np.random.binomial(1, noise_rate, self.codeword_len))
+        print(np.sum(error == 1), "errors in codeword")
+        codeword = (generator_matrix @ secret + one_time_pad + error)
+        return codeword
+    
+    def Decode(self, decoding_key, codeword): 
+        parity_check_matrix, one_time_pad = decoding_key
+        codeword = codeword + one_time_pad
+        threshold = (1/2 - self.num_parity_checks ** (-0.25)) * self.num_parity_checks
+        syndrome = parity_check_matrix @ codeword
+        failed_parity_checks = np.sum(syndrome == 1)
+        print("threshold:", round(threshold, 1) , "failed parity checks:", failed_parity_checks)
+        return failed_parity_checks < threshold    
+
+    def print_parameter_info(self): 
+        print("codeword_len:", self.codeword_len)
+        print("sparsity:", self.sparsity)
+        print("secret_len:", self.secret_len)
+        print("num_parity_checks:", self.num_parity_checks)
+
+
+
+
+    
+
+
