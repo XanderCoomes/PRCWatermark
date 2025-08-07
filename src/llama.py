91521d551f19488be70100c1ad9c3038010d8b15
Xander Coomes <xander.coomes@gmail.com>
2025-08-07 09:26:30 -0700

    clearing dependencies accross folders

--- src/llama.py
+++ src/llama.py
@@ -1,13 +1,17 @@
 from llm import LLM
 from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
 
 class Llama(LLM):
     def __init__(self):
         model_id = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
         tokenizer = AutoTokenizer.from_pretrained(model_id)
-        model = AutoModelForCausalLM.from_pretrained(model_id)
+        model = AutoModelForCausalLM.from_pretrained(
+            model_id,
+            device_map="auto",
+            torch_dtype=torch.float16
+        )
         super().__init__(tokenizer, model)
-
 
 
     
--- src/llm.py
+++ src/llm.py
@@ -3,24 +3,35 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 import torch.nn.functional as F
 
- 
 class LLM: 
     def __init__(self, tokenizer, model): 
         self.tokenizer = tokenizer
         self.model = model
         self.model.eval()
         return 
-    def generate_response(self, prompt, max_tokens): 
-        return
-    def generate_watermarked_response(self, prompt, max_tokens, encoding_key): 
+    def gen_response(self, prompt, max_tokens): 
+        input_ids = self.tokenizer(prompt.strip(), return_tensors="pt")
+        output = self.model.generate(
+            **input_ids,
+            max_new_tokens = max_tokens,
+            do_sample = True,
+            temperature = 0.85,
+            top_p = 0.9,
+            repetition_penalty = 1.15,
+            no_repeat_ngram_size = 3,
+            eos_token_id = self.tokenizer.eos_token_id
+        )
+        response = self.tokenizer.decode(output[0], skip_special_tokens=True)
+        return response
+    def gen_watermarked_response(self, prompt, max_tokens, encoding_key): 
         return
     
     def detect_watermarked_response(self, response, decoding_key):
         return
     
-    def generate_token_logits(self, prompt):
+    def gen_token_logits(self, prompt):
         return
     
-    def calculate_response_entropy(self, prompt, max_tokens, response): 
+    def calc_response_entropy(self, prompt, max_tokens, response): 
         return
     
